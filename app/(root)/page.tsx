5e2e3da226e8a1b34307ebc17dea6ab84abfb3f8
iamharidasan <hdasshp@gmail.com>
2025-04-25 14:26:29 +0530

    Basic Setup of Themes with Dark and Light Mode toggle

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+export const metadata = {
+  title: "Homepage",
+}
+
+const Homepage = () => {
+  return <Button>FPStore</Button>
+}
+
+export default Homepage

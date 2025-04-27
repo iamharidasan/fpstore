9dce57ff1ddf91dc9beafc27b8b20ea8a79dba9f
iamharidasan <hdasshp@gmail.com>
2025-04-27 21:12:24 +0530

    Prisma and PostgresDB has been added and the data from the DB

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,15 @@
-import sampleData from "@/db/sample-data"
 import ProductList from "@/components/shared/product/product-list"
+import { getLatestProducts } from "@/lib/actions/product.actions"
 
 export const metadata = {
   title: "Homepage",
 }
 
-const Homepage = () => {
+const Homepage = async () => {
+  const latestProducts = await getLatestProducts()
   return (
     <>
-      <ProductList
-        data={sampleData.products}
-        title="Newest Arrivals"
-        limit={4}
-      />
+      <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
     </>
   )
 }
---
+++ db/seed.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client"
+import sampleData from "./sample-data"
+
+async function main() {
+  const prisma = new PrismaClient()
+
+  // Clear existing data
+  await prisma.product.deleteMany()
+
+  // Seed products
+  await prisma.product.createMany({ data: sampleData.products })
+
+  console.log("DB seeded successfully.")
+}
+
+main()
